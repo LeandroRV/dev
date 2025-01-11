d324db5f371c8b5c2e926da35663db6f79c41d2f
leandrorv <leandro_rv@outlook.com>
2025-01-11 12:21:41 -0300

    delecao de imagens

--- js/plugins.js
+++ js/plugins.js
@@ -3,7 +3,6 @@
  *
  * ------------------------------------------------------------------- */
 
-
 /* PrismJS 1.20.0
  * https://prismjs.com/download.html#themes=prism&languages=markup+css+clike+javascript+markup-templating+php 
  */
